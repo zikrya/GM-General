a5df8252836cabde0ab45d503928767c3ad0ed93
zikrya <77081809+zikrya@users.noreply.github.com>
2024-01-06 17:49:05 -0500

    Deploy Issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 
 function App() {
 
